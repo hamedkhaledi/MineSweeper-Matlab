2c51faca3e041ed44f4f2fc14cd72f5cc1bb5743
hamed khaledi <hamedkhaledi@users.noreply.github.com>
2020-12-26 18:03:34 +0330

    update hint
    random hint

--- hint_cb.m
+++ hint_cb.m
@@ -3,19 +3,13 @@ function hint_cb()
     global matrix_flags ;
     m = size(matrix_game,1);
     n = size(matrix_game,2);
-    flag = false;
-    for x = 1:m
-        for y = 1:n
-            if (matrix_game(x,y)>0 && matrix_flags(x,y)==0)
-                c = (x-1)*(n)+y;
-                hit_number(x,y,c);
-                flag = true;
-                break;
-            end
-        end
-        if (flag)
-            break;
-        end
+    x = randi([1 m],1);
+    y = randi([1 n],1);
+    while (matrix_game(x,y)<=0 || matrix_flags(x,y)==1) 
+        x = randi([1 m],1);
+        y = randi([1 n],1);
     end
+    c = (x-1)*(n)+y;
+    hit_number(x,y,c);
 end
 
